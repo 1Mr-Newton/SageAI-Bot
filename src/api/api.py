b74172123d053c9e88b4c44266673df6281c3bf5
1mr-newton <eaidoo015@gmail.com>
2024-04-03 03:59:41 +0000

    improving api

--- src/api/api.py
+++ src/api/api.py
@@ -11,7 +11,12 @@ def get_oai_did() -> str | None:
 
 
 def get_chat_token(oai_did: str) -> str | None:
+    print("Oai did", oai_did)
     session = requests.Session()
     default_headers["oai-device-id"] = oai_did
-    token = session.post(openai_token_url, headers=default_headers).json().get("token")
+    response = session.post(openai_token_url, headers=default_headers)
+    if response.status_code != 200:
+        print(response.text)
+        return
+    token = response.json().get("token")
     return token
